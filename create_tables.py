3e724cbc4f3dc42f63bba2f90ccedb2c79b26a88
Ahmed Abid <aabid0193@gmail.com>
2019-05-18 16:17:41 +0400

    added docstrings to create_tables file

--- create_tables.py
+++ create_tables.py
@@ -3,6 +3,16 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def create_database():
+    """
+    Main function to create the database.
+
+    Args:
+        None
+
+    Returns:
+        None
+    """
+
     # connect to default database
     conn = psycopg2.connect("host=127.0.0.1 dbname=studentdb user=student password=student")
     conn.set_session(autocommit=True)
@@ -23,18 +33,50 @@ def create_database():
 
 
 def drop_tables(cur, conn):
+    """
+    This function drops tables from psql database.
+
+    Args:
+        cur (psql cursor): connection cursor for postgres db.
+        conn (psql connection): connection for postgres db using psycopg2.
+
+    Returns:
+        None
+    """
+
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    """
+    This function creates tables using sql_queries.py in the psql database.
+
+    Args:
+        cur (psql cursor): connection cursor for postgres db.
+        conn (psql connection): connection for postgres db using psycopg2.
+
+    Returns:
+        None
+    """
+
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """
+    Main function to create the tables in the database and that database itself.
+
+    Args:
+        None
+
+    Returns:
+        None
+    """
+
     cur, conn = create_database()
 
     drop_tables(cur, conn)
